4f2daddbe74ec4622134bbc21db347010a8f489b
AdityaNair07 <nairaditya07@gmail.com>
2024-12-09 09:07:13 +0530

    meta tag changes

--- src/components/HelmetComponent.jsx
+++ src/components/HelmetComponent.jsx
@@ -7,10 +7,10 @@ const HelmetComponent = ({ product }) => {
       <Helmet>
         <title>{`${product?.title}`} - Buy Online</title>
         <meta name="description" content={product?.description} />
-        <meta property="og:title" content={product?.name} />
+        <meta property="og:title" content={product?.title} />
         <meta property="og:description" content={product?.description} />
         <meta property="og:image" content={product?.images[0]} />
-        <meta property="og:type" content="product" />
+        <meta property="og:type" content="website" />
         <meta
           property="og:url"
           content={`https://dummyjson.com/products/${product?.id}`}
